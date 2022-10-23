451b9256b9de0f3dfbb9e65877d0600adb99097e
vdub <vungh.waan@gmail.com>
2022-10-23 14:18:10 -0500

    Created a variable for the value options.

--- Develop/script.js
+++ Develop/script.js
@@ -36,6 +36,11 @@ let specChar = window.confirm("Click OK to confirm including special characters.
 let lowercase = window.confirm("Click OK to confirm including lowercase characters.");
 let uppercase = window.confirm("Click OK to confirm including uppercase characters.");
 
+// 3. Created a variable for the value options.
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 }
 
